86c9f62b5041508a82d9d6d392ab2f73136f6591
Nicola Di Mauro <nicola.dimauro@uniba.it>
2018-06-06 12:04:32 +0200

    gauss features

--- src/dataset.h
+++ src/dataset.h
@@ -35,12 +35,13 @@ OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 void split (const std::string &, char, std::vector < std::string > &);
 int splits (const std::string &, char);
 
+template <class T>
 class dataset
 {
  public:
   unsigned int shape[2];
-  std::vector < std::vector < int >>lil_data;
-  std::vector < std::vector < int >>data;
+  std::vector < std::vector < std::pair<int,T> >>lil_data;
+  std::vector < std::vector < T >>data;
   double sparsity;
 
   dataset (const std::string);
@@ -50,6 +51,85 @@ class dataset
   ~dataset ()
     {
     }
+ private:
+  T ato(const char*);
 };
+
+template<>
+int dataset<int>::ato(const char* v)
+{
+  return atoi(v);
+}
+
+template<>
+double dataset<double>::ato(const char* v)
+{
+  return atof(v);
+}
+
+template <class T>
+dataset<T>::dataset (const std::string file_name)
+{
+  std::ifstream data_file;
+  std::string line;
+
+
+  std::cout << "Loading data: ";
+
+  int rows = 0, cols = 0;
+
+  data_file.open (file_name);
+
+  SOFT_ASSERT (data_file, " error: " + file_name + " not found!");
+
+  while (getline (data_file, line))
+    {
+      if (line.length ())
+        rows++;
+      if (rows == 1)
+        cols = splits (line, ',');
+    }
+  data_file.close ();
+
+
+  std::cout << rows << " and " << cols << " cols" << std::endl;
+  data.resize (rows);
+  for (int i = 0; i < rows; i++)
+    data[i].resize (cols);
+
+  int r = 0;
+  std::vector < std::string > tokens;
+  tokens.resize (cols);
+  data_file.open (file_name);
+  if (data_file)
+    {
+      while (getline (data_file, line))
+        if (line.length ())
+          {
+            split (line, ',', tokens);
+            for (unsigned int i = 0; i < tokens.size (); i++)
+              data[r][i] = ato (tokens[i].c_str ());
+            r++;
+          }
+      data_file.close ();
+    }
+  shape[0] = data.size ();
+  shape[1] = data[0].size ();
+
+  sparsity = 0;
+  for (unsigned int i = 0; i < shape[0]; i++)
+    {
+      std::vector < std::pair<int,T> >example;
+      for (unsigned int j = 0; j < shape[1]; j++)
+        if (data[i][j] != 0)
+          {
+            example.push_back (std::make_pair(j, data[i,j]));
+            sparsity++;
+          }
+      lil_data.push_back (example);
+    }
+  sparsity /= shape[0] * shape[1];
+}
+
 
 #endif
